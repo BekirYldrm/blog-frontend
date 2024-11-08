94021c9f5f15a39fa8214b5bee96f6018770b471
Bekir <bekiryildirim.2508@gmail.com>
2024-11-08 19:04:12 +0300

    Added new components and styles for About and Blogs sections

---
+++ src/components/BlogsSection/BlogsSection.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+import styles from "/src/styles/BlogsSection.module.css"
+import Blogs from "./Blogs"
+import Categories from "./Categories"
+import { Blog, Category } from "../../types/types"
+import axios from "axios"
+
+const BlogsSection = () => {
+
+  const [categories, setCatogeries] = useState<Category[]>([])
+  const [blogs, setBlogs] = useState<Blog[]>([])
+
+  useEffect(() => {
+    axios
+      .get('https://blog-backend-y4dr.onrender.com/categories')
+      .then(res => setCatogeries(res.data))
+      .catch(e => console.log(e))
+  }, [])
+
+  function clicked(blogs: Blog[]): void {
+    setBlogs(blogs)
+  }
+
+  return (
+    <section id='blogs'>
+      <div className={styles.container}>
+        <Categories categories={categories} clicked={clicked} />
+        <Blogs blogs={blogs} />
+      </div>
+    </section>
+  )
+}
+
+export default BlogsSection
