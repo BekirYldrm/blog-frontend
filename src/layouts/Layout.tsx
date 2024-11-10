587cdea73aac6ddb8f4487819a3e1a0cfce25a95
Bekir <bekiryildirim.2508@gmail.com>
2024-11-11 00:41:43 +0300

    modified file

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,12 +1,17 @@
-import { LayoutProps } from '../types/types'
-import Navbar from './Navbar'
-import styles from '/src/styles/Layout.module.css'
+import { BlogProvider } from '../context/BlogContext';
+import { LayoutProps } from '../types/types';
+import Navbar from './Navbar';
+import styles from '/src/styles/Layout.module.css';
 
 const Layout = ({ children }: LayoutProps) => {
+
     return (
         <div className={styles.container}>
-            <Navbar />
-            <main>{children}</main>
+            <BlogProvider>
+                <Navbar />
+                <main style={{ margin: 0 }}>{children}</main>
+            </BlogProvider>
+
         </div>
     )
 }
