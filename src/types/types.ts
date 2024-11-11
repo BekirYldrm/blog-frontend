acb501f8426c8b6e8085132cd797b4ccadf3a470
Bekir <bekiryildirim.2508@gmail.com>
2024-11-11 20:55:56 +0300

    added new interface

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,18 @@
 import { ReactNode } from "react";
 
+export interface ContextType {
+    blogs: Blog[]
+    categories: Category[]
+    showCategories: boolean
+    setBlogs: React.Dispatch<React.SetStateAction<Blog[]>>
+    setCatogeries: React.Dispatch<React.SetStateAction<Category[]>>
+    setShowCategories: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export interface ContextProviderProps {
+    children: ReactNode;
+}
+
 export interface LayoutProps {
     children: ReactNode
 }
@@ -7,7 +20,7 @@ export interface LayoutProps {
 export interface CategoriesProps {
     categories: Array<Category>;
     clicked: (blogs: Blog[]) => void;
-    toggleDrawer: () => void;  
+    toggleDrawer: () => void;
 }
 
 export interface Category {
