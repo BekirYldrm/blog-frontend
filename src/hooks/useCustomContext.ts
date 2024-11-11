acc875b24f8e82615a8012ceb6f158c1071fe2b5
Bekir <bekiryildirim.2508@gmail.com>
2024-11-11 20:52:39 +0300

    Add custom hook useCustomContext for context management

---
+++ src/hooks/useCustomContext.ts
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+import { Context } from '../contexts/Context';
+
+export const useCustomContext = () => {
+    const context = useContext(Context);
+    if (!context) {
+        throw new Error('Context must be used with in a ContextProvider');
+    }
+    return context;
+};
