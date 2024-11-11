764e180594229291b2730297832e6db74cf038e5
Bekir <bekiryildirim.2508@gmail.com>
2024-11-11 20:55:37 +0300

    used ContextProvider

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,16 +1,16 @@
-import { BlogProvider } from '../context/BlogContext';
+import { ContextProvider } from '../contexts/ContextProvider';
 import { LayoutProps } from '../types/types';
-import Navbar from './Navbar';
+import Navbar from '../components/Navbar/Navbar';
 import styles from '/src/styles/Layout.module.css';
 
 const Layout = ({ children }: LayoutProps) => {
 
     return (
         <div className={styles.container}>
-            <BlogProvider>
+            <ContextProvider>
                 <Navbar />
                 <main style={{ margin: 0 }}>{children}</main>
-            </BlogProvider>
+            </ContextProvider>
 
         </div>
     )
