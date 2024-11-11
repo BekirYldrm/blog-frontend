7a895bf8ce40847c8d50a256fb114bf02114f851
Bekir <bekiryildirim.2508@gmail.com>
2024-11-11 20:43:39 +0300

    grid structure added

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,36 +1,67 @@
-import { Button } from '@mui/material'
-import ShopIcon from '@mui/icons-material/Shop'
-import AppleIcon from '@mui/icons-material/Apple'
-import styles from '/src/styles/AboutSection.module.css'
+import AppleIcon from '@mui/icons-material/Apple';
+import ShopIcon from '@mui/icons-material/Shop';
+import { Button, Tooltip, Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid2';
 
 const About = () => {
+
   return (
+
     <section id='about'>
-      <div className={styles.container}>
-        <div>
-          <p>
+
+      <Grid container mb={10} spacing={2}>
+
+        <Grid size={{ xs: 12, md: 4, lg: 3 }} justifyContent='center' alignContent='center' p={2} >
+          <Typography fontFamily='roboto' variant='h6'>
             Farklı konularda yazılar yazan yetenekli yazarlarımızın ve her yaştan,
             her kesimden okuyucunun bir araya geldiği dinamik bir platform.
             Amacımız, sizlere ilham vermek, güncel ve ilgi çekici içerikler sunarak bilgi
             ve eğlenceyi bir araya getirmek. Keyifli okumalar ve katkılarınız için şimdiden teşekkür ederiz!
-          </p>
-        </div>
-        <img src="/about.png" alt="about" width='55%' />
-        <div className={styles.rightDiv}>
-          <h1>229</h1>
-          <p>Toplam Kullanıcı Sayısı</p>
-          <div>
-            <Button variant="contained" >
-              <ShopIcon sx={{ mx: 1 }} />
-              <p>Google Play</p>
-            </Button>
-            <Button variant="contained" >
-              <AppleIcon sx={{ mx: 1 }} />
-              <p>App Store</p>
-            </Button>
-          </div>
-        </div>
-      </div>
+          </Typography>
+        </Grid>
+
+        <Grid size={{ xs: 12, md: 8, lg: 7 }} alignContent='center'>
+          <Box component='img' src='/about.png' alt="about" width={'100%'} />
+        </Grid>
+
+        <Grid container size={{ xs: 12, lg: 2 }} justifyContent={'center'} alignContent='center' textAlign='center'>
+
+          <Grid size={{ md: 4, lg: 12 }}>
+            <Typography variant='h3' m={1}>229</Typography>
+            <Typography variant='subtitle1' m={1}> Toplam Kullanıcı Sayısı</Typography>
+          </Grid>
+
+          <Grid container size={{ md: 4, lg: 12 }} spacing={2} p={1} justifyContent={'center'} alignContent={'center'}>
+            <Tooltip arrow title='Google Play'>
+              <Button color='inherit'
+                sx={{
+                  borderRadius: 5,
+                  '&:hover': {
+                    backgroundColor: 'black', transition: ' 0.6s ease-in-out', color: 'white'
+                  }
+                }}
+              ><ShopIcon sx={{ fontSize: { xs: 30, sm: 40, md: 50, lg: 60 } }} />
+              </Button>
+            </Tooltip>
+
+
+            <Tooltip arrow title='App Store'>
+              <Button color='inherit'
+                sx={{
+                  borderRadius: 5,
+                  '&:hover': {
+                    backgroundColor: 'black', transition: ' 0.6s ease-in-out', color: 'white'
+                  }
+                }}
+              ><AppleIcon sx={{ fontSize: { xs: 30, sm: 40, md: 50, lg: 60 } }} />
+              </Button>
+            </Tooltip>
+
+          </Grid>
+        </Grid>
+      </Grid>
+
     </section>
   )
 }
