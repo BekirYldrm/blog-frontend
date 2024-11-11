a3eb093230f9a571b67930f89672048e82b8d870
Bekir <bekiryildirim.2508@gmail.com>
2024-11-11 21:48:11 +0300

    fixed file

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,15 +1,14 @@
+import Navbar from '../components/Navbar/Navbar';
 import { ContextProvider } from '../contexts/ContextProvider';
 import { LayoutProps } from '../types/types';
-import Navbar from '../components/Navbar/Navbar';
-import styles from '/src/styles/Layout.module.css';
 
 const Layout = ({ children }: LayoutProps) => {
 
     return (
-        <div className={styles.container}>
+        <div>
             <ContextProvider>
                 <Navbar />
-                <main style={{ margin: 0 }}>{children}</main>
+                <main>{children}</main>
             </ContextProvider>
 
         </div>
