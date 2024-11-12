f0adecb2bcea6a2ff1145c41986cd69136b7608b
Bekir <bekiryildirim.2508@gmail.com>
2024-11-12 15:59:55 +0300

    Design and grid structure have been fixed with material UI.

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,65 +1,45 @@
-import AppleIcon from '@mui/icons-material/Apple';
-import ShopIcon from '@mui/icons-material/Shop';
-import { Button, Tooltip, Typography } from '@mui/material';
-import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid2';
+import DownloadButton from './DownloadButton';
+import Text from './Text'
 
 const About = () => {
 
   return (
 
-    <section id='about'>
+    <section id='about' style={{ padding: '0px 16px 16px', margin: '32px 0px' }}>
 
-      <Grid container mb={10} spacing={2}>
+      <Grid container spacing={2}>
 
-        <Grid size={{ xs: 12, md: 4, lg: 3 }} justifyContent='center' alignContent='center' p={2} >
-          <Typography fontFamily='roboto' variant='h6'>
-            Farklı konularda yazılar yazan yetenekli yazarlarımızın ve her yaştan,
-            her kesimden okuyucunun bir araya geldiği dinamik bir platform.
-            Amacımız, sizlere ilham vermek, güncel ve ilgi çekici içerikler sunarak bilgi
-            ve eğlenceyi bir araya getirmek. Keyifli okumalar ve katkılarınız için şimdiden teşekkür ederiz!
-          </Typography>
-        </Grid>
+        <Grid container size={{ xs: 12, lg: 3 }} p={2}
+          justifyContent='center' alignContent='center'
+          sx={{ bgcolor: 'whitesmoke', borderRadius: 2, boxShadow: '0px 4px 10px rgba(0,0,0,0.1)' }}
+        >
 
-        <Grid size={{ xs: 12, md: 8, lg: 7 }} alignContent='center'>
-          <Box component='img' src='/about.png' alt="about" width={'100%'} />
-        </Grid>
-
-        <Grid container size={{ xs: 12, lg: 2 }} justifyContent={'center'} alignContent='center' textAlign='center'>
-
-          <Grid size={{ md: 4, lg: 12 }}>
-            <Typography variant='h3' m={1}>229</Typography>
-            <Typography variant='subtitle1' m={1}> Toplam Kullanıcı Sayısı</Typography>
+          <Grid container size={{ xs: 12, sm: 10, md: 8, lg: 12 }} p={2}>
+            <Text />
           </Grid>
 
-          <Grid container size={{ md: 4, lg: 12 }} spacing={2} p={1} justifyContent={'center'} alignContent={'center'}>
-            <Tooltip arrow title='Google Play'>
-              <Button color='inherit'
-                sx={{
-                  borderRadius: 5,
-                  '&:hover': {
-                    backgroundColor: 'black', transition: ' 0.6s ease-in-out', color: 'white'
-                  }
-                }}
-              ><ShopIcon sx={{ fontSize: { xs: 30, sm: 40, md: 50, lg: 60 } }} />
-              </Button>
-            </Tooltip>
-
-
-            <Tooltip arrow title='App Store'>
-              <Button color='inherit'
-                sx={{
-                  borderRadius: 5,
-                  '&:hover': {
-                    backgroundColor: 'black', transition: ' 0.6s ease-in-out', color: 'white'
-                  }
-                }}
-              ><AppleIcon sx={{ fontSize: { xs: 30, sm: 40, md: 50, lg: 60 } }} />
-              </Button>
-            </Tooltip>
-
+          <Grid container py={{ xs: 2, sm: 0 }} size={{ xs: 12, sm: 2, md: 4, lg: 12 }}
+            justifyContent={'center'} alignContent={'center'}
+          >
+            <DownloadButton title='Google Play' />
+            <DownloadButton title='App Store' />
           </Grid>
+
         </Grid>
+
+        <Grid
+          component='img'
+          src='/about.png'
+          alt="about"
+          size={{ md: 12, lg: 9 }}
+          sx={{
+            borderRadius: 3, boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.2)',
+            transition: 'transform 0.4s ease-in-out',
+            '&:hover': { transform: 'scale(1.02)' }
+          }}
+        />
+
       </Grid>
 
     </section>
