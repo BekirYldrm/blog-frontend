c59fbd03f70dcdfbd6952866b4d5fba8a3ba0ea6
Bekir <bekiryildirim.2508@gmail.com>
2024-11-29 20:27:07 +0300

    added getPrivateData async  function and baseURL

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,36 @@
 import axios, { AxiosResponse } from 'axios';
 
+const api = axios.create({
+    baseURL: 'https://blog-backend-5uhs.onrender.com',
+});
+
 export async function getData<T>(url: string): Promise<T> {
     try {
-        const response: AxiosResponse<T> = await axios.get(url);
+        const response: AxiosResponse<T> = await api.get(url);
+        return response.data;
+    } catch (error) {
+        throw new Error('Failed to fetch data');
+    }
+}
+
+export async function postData<T>(url: string, data: any): Promise<T> {
+    try {
+        const response: AxiosResponse<T> = await api.post(url, data);
+        return response.data;
+    } catch (error) {
+        throw new Error('Failed to post data');
+    }
+}
+
+export async function getPrivateData<T>(url: string, token: string): Promise<T> {
+    try {
+        const response: AxiosResponse<T> = await api.get(url, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
+
+        });
         return response.data;
     } catch (error) {
         throw new Error('Failed to fetch data');
